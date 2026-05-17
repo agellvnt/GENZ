ecf4155f8ce388c246ac37c8a79508fee63fe265
agellvnt <agellvnt@gmail.com>
2026-05-17 21:12:25 +0700

    update roles in testimonials to reflect current positions

--- src/components/landing/Testimonials.jsx
+++ src/components/landing/Testimonials.jsx
@@ -40,7 +40,7 @@ const testimonials = [
     },
     {
         name: "CHY",
-        role: "F&O Trader · Surat",
+        role: "Core Member",
         avatar:
             "https://plus.unsplash.com/premium_photo-1738590017220-5820f49608cc?q=80&w=1267&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         text: "ilmu daging wagyu a5 dari mas @Astana dan @farhan 🔥",
@@ -49,7 +49,7 @@ const testimonials = [
     },
     {
         name: "PRABOBO",
-        role: "Crypto + Equity",
+        role: "core member",
         avatar:
             "https://plus.unsplash.com/premium_photo-1738590017220-5820f49608cc?q=80&w=1267&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         text: "“Masuk GENZ Trade dari zero banget. Sekarang udah ngerti liquidity, narrative, dan market structure dengan cara yang simpel dan mudah dipahami.”",
